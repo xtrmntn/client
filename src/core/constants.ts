89aa48c5961d657607d906c15cfb36d3f7e0a881
Sergei Venukov <shid1095@gmail.com>
2023-03-26 19:36:39 +0500

    new phone const

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -1,7 +1,7 @@
 export const address = '460961, Оренбургская область, г. Оренбург, ул. Автоматики проезд, д. 12/1, кв. 25';
 export const company = 'ДезФловер';
 export const site = 'https://dezflower.ru';
-export const phone = '+7 (961) 903-15-80';
+export const phone = '+7 (905) 843-7-843';
 export const email = 'dezflower@mail.ru';
 export const inn = '560912460800';
 export const ogrn = '321565800030681';
